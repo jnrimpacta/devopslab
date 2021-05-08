10179070a699827e2260dbf38fe1642c5ea09e73
Ademilson Junior <ademilson.junior@aluno.faculdadeimpacta.com.br>
2021-05-08 11:24:44 -0300

    Recap

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Laboratório Pipeline DevOps"
+    break
 
 if __name__ == '__main__':
     app.run(debug=True)
